df315f4758dca978a3d1bc567951007d02b334a1
Loris Leitner <lorisleitner@live.com>
2019-08-16 20:00:39 +0200

    Add LCrypt.CLI project

---
+++ LCrypt.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LCrypt.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
