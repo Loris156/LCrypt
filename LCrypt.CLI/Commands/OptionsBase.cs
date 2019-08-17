dad0054f130f6e578d4197dbebd6766f3e5f1de4
Loris Leitner <lorisleitner@live.com>
2019-08-17 14:59:15 +0200

    Add verbose parameter

--- LCrypt.CLI/Commands/OptionsBase.cs
+++ LCrypt.CLI/Commands/OptionsBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,7 @@ namespace LCrypt.CLI.Commands
 {
     public class OptionsBase
     {
+        [Option('v', "verbose", Default = false, HelpText = "Enables verbose output")]
+        public bool Verbose { get; set; }
     }
 }
