6eebc8a9483469c06335a40b1d50a1ae9c48ad9f
Loris Leitner <lorisleitner@live.com>
2019-08-20 09:18:47 +0200

    Add async key derive method

---
+++ LCrypt.Core/Crypto/Pbkdf2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace LCrypt.Core.Crypto
+{
+    public static class Pbkdf2
+    {
+        public static Task<byte[]> DeriveKeyAsync(string password, byte[] salt, int iterations, int byteCount)
+        {
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            if (salt == null)
+                throw new ArgumentNullException(nameof(salt));
+
+            return Task.Run(() =>
+            {
+                using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations))
+                {
+                    return pbkdf2.GetBytes(byteCount);
+                }
+            });
+        }
+    }
+}
