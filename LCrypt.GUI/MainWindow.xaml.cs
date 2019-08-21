46de04dd47ea2d21e72a9c66a33f438490726a8c
Loris Leitner <lorisleitner@live.com>
2019-08-21 08:37:41 +0200

    Add MahApps.Metro to GUI

--- LCrypt.GUI/MainWindow.xaml.cs
+++ LCrypt.GUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,10 +16,7 @@ using System.Windows.Shapes;
 
 namespace LCrypt.GUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
